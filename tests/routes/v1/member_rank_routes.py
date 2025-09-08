a536d57304ec42426ee7f289eedac17ecd5f8887
Jennifer Wallace <jennikate@gmail.com>
2025-09-08 15:01:05 +0100

    Add post member test

--- tests/routes/v1/member_rank_routes.py
+++ tests/routes/v1/member_rank_routes.py
@@ -24,27 +24,33 @@ class TestPostMember:
         """
         Tests that a user can post a new member to the API.
         """
-        print("------------- Testing POST /v1/member endpoint -------------")
-        id = str(sample_ranks[0].id) # Get the id of the first sample rank & make it a string so we can pass it to the POST
-        print(f"Using rank id: {id} for the new member")
+        rank_id = str(sample_ranks[0].id) # Get the id of the first sample rank & make it a string so we can pass it to the POST
         new_member = {
             "name": "Member Name",
-            "rank_id": id,
+            "rank_id": rank_id,
         }
-        print(f"Using details: {new_member} for the new member")
+        
         response = client.post("/v1/member", json=new_member)
         data = response.get_json()
-        print(f"Response data: {data}")
 
-        # assert response.status_code == 201
-        # current_app.logger.info(f"Response data: {data}")
-        # assert data["id"] == data["id"]  # Check that an id is returned
-        # assert data["name"] == "Member Name"
-        # assert data["status"] == True
-        # assert data["rank"] == id
+        expected_response = {
+            "id": data["id"], # we can't predict the id, so just check it's there
+            "name": "Member Name",
+            "rank": {
+                "id": rank_id,
+                "name": "Captain",
+                "position": 1,
+                "share": 1.0
+            },
+            "status": True
+        }
 
-        # # fetch the actual model from the DB
-        # rank = MemberModel.query.get(data["id"])
+        assert response.status_code == 201
+        assert response.get_json() == expected_response
 
-        # expected_repr = f"<RankModel(id={data["id"]}, name='Test Rank', position=1, share=0.1)>"
-        # assert repr(rank) == expected_repr
+        # fetch the actual model from the DB
+        rank = MemberModel.query.get(data["id"])
+
+        expected_repr = f"<MemberModel(id={data["id"]}, name='Member Name', rank=<RankModel(id={rank_id}, name='Captain', position=1, share=1.0)>)>"
+        assert repr(rank) == expected_repr
+    
