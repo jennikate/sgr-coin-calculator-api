71a5b8a5f3e16be4895a14dad6f36b329d588c91
Jennifer Wallace <jennikate@gmail.com>
2025-09-10 14:35:17 +0100

    Update tests for patch

--- tests/routes/v1/test_member_routes.py
+++ tests/routes/v1/test_member_routes.py
@@ -194,154 +194,134 @@ class TestGetMembers:
 
 @pytest.mark.usefixtures("sample_members", "sample_ranks")
 class TestUpdateMember:
-    # def test_update_member(self, client, sample_members, sample_ranks):
-    #     """
-    #     Tests that a user can update a member in the API.
-    #     """
-    #     id = sample_members[0].id # Get the id of a sample member
-    #     # verify details of the member before updates
-    #     original_response = client.get(f"/v1/member/{id}")
-    #     original_expected_response = {
-    #             "id": str(sample_members[0].id),
-    #             "name": str(sample_members[0].name),
-    #             "rank": {
-    #                 "id": str(sample_ranks[0].id),
-    #                 "name": str(sample_ranks[0].name),
-    #                 "position": int(sample_ranks[0].position),
-    #                 "share": float(sample_ranks[0].share)
-    #             },
-    #             "status": bool(sample_members[0].status)
-    #         }
-    #     assert original_response.get_json() == original_expected_response
-    #     # update the member
-    #     updated_member = {
-    #         "name": "Updated Member Name",
-    #         "rank_id": str(sample_ranks[2].id), # change to a different rank
-    #         "status": False
-    #     }
-    #     update_response = client.patch(f"/v1/member/{id}", json=updated_member)
-    #     updated_expected_response = {
-    #             "id": str(sample_members[0].id),
-    #             "name": "Updated Member Name",
-    #             "rank": {
-    #                 "id": str(sample_ranks[2].id),
-    #                 "name": str(sample_ranks[2].name),
-    #                 "position": int(sample_ranks[2].position),
-    #                 "share": float(sample_ranks[2].share)
-    #             },
-    #             "status": False
-    #         }
-    #     assert update_response.status_code == 200
-    #     assert update_response.get_json() == updated_expected_response
-
-    # def test_update_member_name_only(self, client, sample_members, sample_ranks):
-    #     """
-    #     Tests that a user can update a member name in the API.
-    #     """
-    #     id = sample_members[0].id # Get the id of a sample member
-    #     # verify details of the member before updates
-    #     original_response = client.get(f"/v1/member/{id}")
-    #     original_expected_response = {
-    #             "id": str(sample_members[0].id),
-    #             "name": str(sample_members[0].name),
-    #             "rank": {
-    #                 "id": str(sample_ranks[0].id),
-    #                 "name": str(sample_ranks[0].name),
-    #                 "position": int(sample_ranks[0].position),
-    #                 "share": float(sample_ranks[0].share)
-    #             },
-    #             "status": bool(sample_members[0].status)
-    #         }
-    #     assert original_response.get_json() == original_expected_response
-
-    #     # update the member
-    #     updated_member = {
-    #         "name": "Updated Member Name"
-    #     }
-    #     update_response = client.patch(f"/v1/member/{id}", json=updated_member)
-    #     updated_expected_response = {
-    #             "id": str(sample_members[0].id),
-    #             "name": "Updated Member Name",
-    #             "rank": {
-    #                 "id": str(sample_ranks[0].id),
-    #                 "name": str(sample_ranks[0].name),
-    #                 "position": int(sample_ranks[0].position),
-    #                 "share": float(sample_ranks[0].share)
-    #             },
-    #             "status": bool(sample_members[0].status)
-    #         }
-    #     assert update_response.status_code == 200
-    #     assert update_response.get_json() == updated_expected_response
-
-    def test_update_member_rank_only(self, client, db, sample_members, sample_ranks):
+    def test_update_member(self, client, sample_members, sample_ranks):
         """
-            Tests that a user can update a member in the API.
+        Tests that a user can update a member in the API.
         """
         member_id = sample_members[0].id # Get the id of a sample member
-        # confirm original rank is not the one we will update to
+        # confirm original is not the one we will update to
+        assert sample_members[0].name != "Updated"
         assert sample_members[0].rank_id != sample_ranks[2].id
+        assert sample_members[0].status != False
 
         patch_data = {
             "name": "Updated",
-            "rank_id": str(sample_ranks[2].id)
+            "rank_id": str(sample_ranks[2].id),
+            "status": False
         }
-        print('patch rank', patch_data['rank_id'])
+
         response = client.patch(f"/v1/member/{member_id}", json=patch_data)
         assert response.status_code == 200
         data = response.get_json()
-        print('data rank id:', data['rank']['id'])
 
-        # expected_result = {
-        #     'id': '5f1f108c-5eca-4190-8e63-10eea14f873f', 
-        #     'name': 'Alice Updated', 
-        #     'rank': {
-        #         'id': '28416ae5-f89d-43d8-9ad5-89b966d04ffd', 
-        #         'name': 'Lieutenant', 
-        #         'position': 2, 
-        #         'share': 1.0
-        #         }, 
-        #     'status': True
-        # }
-        assert data["name"] == "Updated"
-        assert data["rank"]["id"] == str(sample_ranks[2].id)
+        expected_result = {
+            'id': str(member_id), 
+            'name': "Updated", 
+            'rank': {
+                'id': str(sample_ranks[2].id), 
+                'name': str(sample_ranks[2].name), 
+                'position': int(sample_ranks[2].position), 
+                'share': float(sample_ranks[2].share)
+                }, 
+            'status': False
+        }
+        
+        assert expected_result == data
 
-    # def test_update_member_status_only(self, client, sample_members, sample_ranks):
-    #     """
-    #         Tests that a user can update a member in the API.
-    #     """
-    #     id = sample_members[0].id # Get the id of a sample member
-    #     # verify details of the member before updates
-    #     original_response = client.get(f"/v1/member/{id}")
-    #     original_expected_response = {
-    #             "id": str(sample_members[0].id),
-    #             "name": str(sample_members[0].name),
-    #             "rank": {
-    #                 "id": str(sample_ranks[0].id),
-    #                 "name": str(sample_ranks[0].name),
-    #                 "position": int(sample_ranks[0].position),
-    #                 "share": float(sample_ranks[0].share)
-    #             },
-    #             "status": bool(sample_members[0].status)
-    #         }
-    #     assert original_response.get_json() == original_expected_response
-    #     # update the member
-    #     updated_member = {
-    #         "status": False
-    #     }
-    #     update_response = client.patch(f"/v1/member/{id}", json=updated_member)
-    #     updated_expected_response = {
-    #             "id": str(sample_members[0].id),
-    #             "name": str(sample_members[0].name),
-    #             "rank": {
-    #                 "id": str(sample_ranks[0].id),
-    #                 "name": str(sample_ranks[0].name),
-    #                 "position": int(sample_ranks[0].position),
-    #                 "share": float(sample_ranks[0].share)
-    #             },
-    #             "status": False
-    #         }
-    #     assert update_response.status_code == 200
-    #     assert update_response.get_json() == updated_expected_response
+    def test_update_member_name_only(self, client, sample_members, sample_ranks):
+        """
+        Tests that a user can update a member name only in the API.
+        """
+        member_id = sample_members[0].id
+        # confirm original is not the one we will update to
+        assert sample_members[0].name != "Updated"
+        # get the original member details to assert against
+        original_member = client.get(f"/v1/member/{member_id}")
+
+        patch_data = {
+            "name": "Updated"
+        }
+
+        response = client.patch(f"/v1/member/{member_id}", json=patch_data)
+        assert response.status_code == 200
+        data = response.get_json()
+
+        expected_result = {
+            'id': str(member_id), 
+            'name': "Updated", 
+            'rank': {
+                'id': original_member.get_json()['rank']['id'], 
+                'name': original_member.get_json()['rank']['name'], 
+                'position': original_member.get_json()['rank']['position'], 
+                'share': original_member.get_json()['rank']['share']
+                }, 
+            'status': original_member.get_json()['status']
+        }
+        
+        assert expected_result == data
+
+    def test_update_member_rank_only(self, client, db, sample_members, sample_ranks):
+        """
+            Tests that a user can update a member rank only in the API.
+        """
+        member_id = sample_members[0].id # Get the id of a sample member
+        # confirm original is not the one we will update to
+        assert sample_members[0].rank_id != sample_ranks[2].id
+
+        patch_data = {
+            "rank_id": str(sample_ranks[2].id)
+        }
+
+        response = client.patch(f"/v1/member/{member_id}", json=patch_data)
+        assert response.status_code == 200
+        data = response.get_json()
+
+        expected_result = {
+            'id': str(member_id), 
+            'name': sample_members[0].name, 
+            'rank': {
+                'id': str(sample_ranks[2].id), 
+                'name': str(sample_ranks[2].name), 
+                'position': int(sample_ranks[2].position), 
+                'share': float(sample_ranks[2].share)
+                }, 
+            'status': sample_members[0].status
+        }
+        
+        assert expected_result == data
+
+    
+    def test_update_member_status_only(self, client, sample_members, sample_ranks):
+        """
+            Tests that a user can update a member status only in the API.
+        """
+        member_id = sample_members[0].id
+        # confirm original is not the one we will update to
+        assert sample_members[0].name != "Updated"
+        # get the original member details to assert against
+        original_member = client.get(f"/v1/member/{member_id}")
+
+        patch_data = {
+            "status": False
+        }
+
+        response = client.patch(f"/v1/member/{member_id}", json=patch_data)
+        assert response.status_code == 200
+        data = response.get_json()
+
+        expected_result = {
+            'id': str(member_id), 
+            'name': original_member.get_json()['name'], 
+            'rank': {
+                'id': original_member.get_json()['rank']['id'], 
+                'name': original_member.get_json()['rank']['name'], 
+                'position': original_member.get_json()['rank']['position'], 
+                'share': original_member.get_json()['rank']['share']
+                }, 
+            'status': False
+        }
+        
+        assert expected_result == data
 
 @pytest.mark.usefixtures("sample_members", "sample_ranks")
 class TestDeleteMember:
