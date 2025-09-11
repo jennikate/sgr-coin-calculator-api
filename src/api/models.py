1fec72a63bbeee227069dc0d11f9384bd7611eab
Jennifer Wallace <jennikate@gmail.com>
2025-09-11 12:17:49 +0100

    Recreate db with default rank id

--- src/api/models.py
+++ src/api/models.py
@@ -40,6 +40,8 @@ class RankModel(db.Model):
     members = db.relationship('MemberModel', back_populates='rank')
     # could do cascade='all, delete-orphan which would delete all associated members if a rank is deleted
     # but I don't want to lose the members so this isn't useful here
+    # instead we have `ondelete='SET NULL'`` in the MemberModel and we update it to a DEFAULT_VALUE
+    # when we delete the rank
     # members = db.relationship('Member', back_populates='rank', cascade='all, delete-orphan')
 
     
@@ -68,7 +70,12 @@ class MemberModel(db.Model):
     status = db.Column(db.Boolean, default=True, nullable=False) # active/inactive status
 
     # foreign key to ranks table
-    rank_id = db.Column(db.UUID, db.ForeignKey('ranks.id'), nullable=False)
+    rank_id = db.Column(
+        db.UUID, 
+        db.ForeignKey('ranks.id', ondelete='SET NULL'), # temporarily set to NULL so we can then assign a DEFAULT_RANK
+        nullable=False,
+        default='11111111-1111-1111-1111-111111111111'
+    )
 
     # relationship for easy access
     rank = db.relationship('RankModel', back_populates='members')
@@ -81,4 +88,3 @@ class MemberModel(db.Model):
 ###################################################################################################
 # End of file
 ###################################################################################################
-
